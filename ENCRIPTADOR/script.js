47da3bf720af39d899be372d2d6484579a1e23e5
Filipe Souza Menezes <101118065+menezesfilipe@users.noreply.github.com>
2023-05-23 13:05:14 -0300

    adicionando projeto

---
+++ ENCRIPTADOR/script.js
@@ -0,0 +1,40 @@
+const textArea = document.querySelector(".textarea")
+const mensagem = document.querySelector(".mensagem")
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensagem.value = textoEncriptado;
+    textArea.value = "";
+}
+
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"] , ["i", "imes"] , ["a", "ai"] , ["o", "obes"] , ["u", "ufat"]];
+
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0]))
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i] [1])
+    }
+    return stringEncriptada
+}
+
+function btnDesencriptar(){
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensagem.value = textoDesencriptado;
+    textArea.value = "";
+}
+
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e", "enter"] , ["i", "imes"] , ["a", "ai"] , ["o", "obes"] , ["u", "ufat"]];
+
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][0]))
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i] [0])
+    }
+    return stringDesencriptada
+}
